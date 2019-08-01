3e320c5ebc355d71c61b692ba887a002fe2a111d
pchatterjee19 <53345693+pchatterjee19@users.noreply.github.com>
2019-07-31 20:44:46 -0400

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,3 +1,8 @@
+az login -u partha.chatterjee@jvion.com -p Stepover@17
+terraform init
 terraform state list >> test.txt
-sh test.sh test.txt
-terraform apply -auto-approve
+while IFS= read -r line; do
+    echo "Text read from file: $line"
+    terraform state rm $line
+done < "test.txt"
+terraform apply -var="suffix=ned_stark" -var="resource_group_name=data-science-rg" -var="virtual_net=data-science-vnet" -var="subnet=virtual-machines" -auto-approve
